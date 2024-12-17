da6ebeb2bb724418c058a859237fb5a85ceb6dc9
buyaramadhan <ytchochote@gmail.com>
2024-12-17 07:24:09 +0700

    menambah UpdateMhsViewModel

---
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.pertemuan10.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan10.data.entity.Mahasiswa
+import com.example.pertemuan10.repository.RepositoryMhs
+import com.example.pertemuan10.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMhsViewModel(
+   )
