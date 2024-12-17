0e9274188077961a564eba03a3de18e4b48da836
buyaramadhan <ytchochote@gmail.com>
2024-12-17 08:03:03 +0700

    menambah AlamatNavigasi

--- app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,17 @@ package com.example.pertemuan10.ui.navigation
 interface AlamatNavigasi {
     val route : String
 }
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
