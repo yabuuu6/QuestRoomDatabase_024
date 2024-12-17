bf8936cc4dda9717a9a37b065d3e4af28061e8a9
buyaramadhan <ytchochote@gmail.com>
2024-12-17 07:27:37 +0700

    menambah fun updatestate

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -16,4 +16,25 @@ import kotlinx.coroutines.launch
 class UpdateMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
-)
+) : ViewModel() {
+    var updateUiState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        updateUiState = updateUiState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
+
+}
