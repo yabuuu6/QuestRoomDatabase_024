fda2f47379fb1804bad81d1747e42e8cbd2d87d9
buyaramadhan <ytchochote@gmail.com>
2024-12-17 07:37:16 +0700

    menambah BodyHomeMhsView

--- app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/HomeMhsView.kt
@@ -80,3 +80,60 @@ fun HomeMhsView(
     }
 }
 
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } //Snackbar stste
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box (modifier= modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center)
+            {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            //menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let{message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)  //menampilkan snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listMhs.isEmpty() -> {
+            //menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data mahasiswa.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            //Menampilkan daftar mahasiswa
+            ListMahasiswa(
+                listMhs = homeUiState.listMhs,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = Modifier.padding(top = 100.dp)
+            )
+        }
+    }
+}
+
