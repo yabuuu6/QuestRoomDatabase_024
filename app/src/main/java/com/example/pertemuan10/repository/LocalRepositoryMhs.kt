7db4469fcb460f7485e2e3bc8560a1179781d359
buyaramadhan <ytchochote@gmail.com>
2024-12-17 08:04:53 +0700

    menambah override fun getallmhs

--- app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
@@ -2,12 +2,29 @@ package com.example.pertemuan10.repository
 
 import com.example.pertemuan10.data.dao.MahasiswaDao
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
-) : RepositoryMhs{
-    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-       mahasiswaDao.insertMahasiswa(mahasiswa)
+):RepositoryMhs{
+    override suspend fun  insertMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.insertMahasiswa(mahasiswa)
     }
 
+    //get all mahasiswa
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {  //mengambil data mahasiswa berdasarkan nim
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) { //menghapus data mahasiswa
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) { //memperbarui data mahasiswa dalam database
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
 }
