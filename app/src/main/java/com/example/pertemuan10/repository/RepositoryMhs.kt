fd371615d312fb4229bcb8a80c049bbd40b9c675
buyaramadhan <ytchochote@gmail.com>
2024-12-17 08:07:18 +0700

    menambah fun getAllmhs dan getMhs

--- app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
@@ -1,7 +1,17 @@
 package com.example.pertemuan10.repository
 
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
 
 interface RepositoryMhs {
     suspend fun  insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+
+    fun getMhs (nim: String) : Flow<Mahasiswa>
+
+    suspend fun  deleteMhs (mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs (mahasiswa: Mahasiswa)
 }
